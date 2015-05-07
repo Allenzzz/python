931fa7a05d760c94b323260c0374ef7a585e3b58
Allen,Zhang <jiet2010@126.com>
2015-05-07 14:50:49 +0800

    asdf

---
+++ dict.py
@@ -0,0 +1,14 @@
+#encoding=utf-8
+user={}
+user['name']=[]
+user['id']=[]
+st='abcdefghigklmnopqrst'
+num='0123456789'
+c=0
+for a in ('abcdefghig'):
+	for b in ('klmnopqrst'):
+		#user['name'].append('Allen'+a*2+b*2)
+		
+		user['id'].append(2012+(c=c+1))
+
+print user
---
+++ list.py
@@ -0,0 +1,26 @@
+#encding=utf-8
+
+# li=[]
+# for i in range(10):
+# 	li.append(i*i)
+# print li
+
+# b=[111]
+# li.extend(b)
+# print li
+
+# li.reverse()
+# print li
+
+# print li.reverse() # return None
+
+'''
+	append	用于在列表末尾追加新对象
+	count	用于统计某个元素在列表中出现的次数
+	extend	在列表末尾一次性追加一个序列（多个值）
+	index	查找在列表中第一个匹配项的索引值
+	pop		移除列表中一个元素（默认为最后一个），并返回该值
+	remove	移除列表中某个值的第一匹配项
+	reverse	反向存放列表的元素，不返回值
+	sort	在原位置对列表排序
+'''
